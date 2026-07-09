42f5f7dd250746c97db850fe4c646c610f525f25
Diego Brocanelli <diegod2@msn.com>
2026-07-08 21:41:32 -0300

    DEBUG

--- install.sh
+++ install.sh
@@ -110,11 +110,11 @@ info "Aplicando estrutura avançada e dependências modernas..."
 
 info "DEBUG: Diretório atual = $(pwd)"
 
-info "${RAW_NAME}"
+info "DEBUG: Entrando no diretório = $(pwd)/${RAW_NAME}"
 
 sleep 3
 
-cd "${RAW_NAME}" || { error "Não foi possível entrar no diretório do projeto"; exit 1; }
+cd "$(pwd)/${RAW_NAME}" || { error "Não foi possível entrar no diretório do projeto"; exit 1; }
 
 info "DEBUG: Diretório atual = $(pwd)"
 
